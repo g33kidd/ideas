00c4f153b3adacad6e85835db833d2925814cb65
Joshua Kidd <kidd.josh.343@gmail.com>
2014-11-08 19:19:53 -0700

    Added watch count to idea page.

--- model/comments.js
+++ model/comments.js
@@ -1,1 +1,26 @@
 Comments = new Mongo.Collection('comments');
+
+Meteor.methods({
+
+	comment: function(commentAttr) {
+		check(commentAttr, {
+			content: String
+		});
+
+		var comment = _.extend(commentAttr, {
+			userId: Meteor.userId(),
+			created: new Date()
+		});
+
+		var inserted = Comments.insert(comment);
+		return { _id: inserted._id };
+	},
+
+	removeComment: function(commentId) {
+		Comments.remove({_id: commentId}, function(error) {
+			if(error)
+				return error.reason;
+		});
+	}
+
+});
